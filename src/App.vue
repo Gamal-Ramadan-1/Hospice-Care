ac3395382f96a6c8915082d133210c8ae95fba50
Gamal-Ramadan-1 <gamal.ramadan.fe@gmail.com>
2022-11-07 13:35:25 +0200

    ContactUs Without Form and Informations

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <Header />
+  <ContactUs />
 </template>
 
 <script>
+import ContactUs from "./components/ContactUs/ContactUs.vue";
 import Header from "./components/Header/Header.vue";
 export default {
-  components: { Header },
+  components: { Header, ContactUs },
   name: "App",
 };
 </script>
---
+++ src/components/ContactUs/ContactUs.vue
@@ -0,0 +1,43 @@
+<template>
+  <section>
+    <div class="layout"></div>
+    <div class="container">
+      <div class="row">
+        <h1 class="text-light text-center py-5">
+          #1 Hospice Care Provider in the Greater Houston Area
+        </h1>
+      </div>
+      <div class="row"></div>
+    </div>
+  </section>
+</template>
+<script>
+export default {
+  name: "ContactUs",
+};
+</script>
+<style lang="scss" scoped>
+@import "@/Scss/Main.scss";
+section {
+  background-image: url("@/Imgs/5a203b3f-611e-4164-b2ba-7be12359e580.png");
+  background-position: center center;
+  background-attachment: fixed;
+  background-size: cover;
+  clear: both;
+  object-fit: cover;
+  height: 800px;
+  position: relative;
+  z-index: 0;
+  .layout {
+    position: absolute;
+    z-index: -1;
+    width: 100%;
+    height: 100%;
+    background-color: $Text-Color;
+    opacity: 0.7;
+  }
+  h1{
+    text-shadow: 0px 0px 2px black;
+  }
+}
+</style>
