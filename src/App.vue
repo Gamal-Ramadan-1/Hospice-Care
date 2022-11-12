69cb1e758ed7fa3983141c729290b4489b93616e
Gamal-Ramadan-1 <gamal.ramadan.fe@gmail.com>
2022-11-12 16:52:30 +0200

    ;'

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
   <Header />
   <ContactUs />
+  <ComfortableCare />
 </template>
 
 <script>
+import ComfortableCare from "./components/ComfortableCare/ComfortableCare.vue";
 import ContactUs from "./components/ContactUs/ContactUs.vue";
 import Header from "./components/Header/Header.vue";
 export default {
-  components: { Header, ContactUs },
+  components: { Header, ContactUs, ComfortableCare },
   name: "App",
 };
 </script>
---
+++ src/components/ComfortableCare/ComfortableCare.vue
@@ -0,0 +1,43 @@
+<template>
+  <section>
+    <div class="container">
+      <div class="row">
+        <div class="col-12 text-center">
+          <h1 class="text-light pt-4 header">
+            Get The Most Comfortable & Compassionate Care
+            <span class="d-lg-block d-inline">
+              That You & Your Loved Ones Deserve</span
+            >
+          </h1>
+          <img
+            src="@/Imgs/68fdcfd8-6343-4cc8-ae00-d76515003c1b.png"
+            class="py-2 img-fluid"
+          />
+          <div class="line"></div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+<script>
+export default {
+  name: "ComfortableCare",
+};
+</script>
+<style lang="scss" scoped>
+@import "@/Scss/Main.scss";
+section {
+  background-color: $Main-Color;
+  .header {
+    font-size: 45px;
+    font-weight: 600;
+  }
+  .line {
+    width: 10%;
+    height: 2px;
+    background-color: transparent;
+    border-top: 1px solid #eee;
+    margin: 5px auto;
+  }
+}
+</style>
