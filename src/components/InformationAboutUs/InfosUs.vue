c468dca0b31d9611d69e0ed1646ddf8c107cd631
Gamal Ramadan <gamal.ramadan.fe@gmail.com>
2022-11-17 19:54:28 +0200

    Content of OurServeces Section

--- src/components/InformationAboutUs/InfosUs.vue
+++ src/components/InformationAboutUs/InfosUs.vue
@@ -1,6 +1,7 @@
 <template>
   <section>
     <div class="container">
+
       <Head Text="about white orchid hospice" />
       <div class="row pb-2">
         <div class="col-lg-12">
@@ -14,9 +15,9 @@
           </h3>
         </div>
       </div>
+
       <Head Text="what you can expect" />
-      <div
-        class="
+      <div class="
           row
           justify-content-center
           align-items-center
@@ -24,37 +25,31 @@
           mb-lg-4
           mt-3
           gy-5
-        "
-      >
+        ">
         <div class="col-lg-4 col-md-6 col-sm-12 text-center">
-          <img
-            src="@/Imgs/location_Jp4vBwWWQJdspq8YagE9_images_5cfb2eb6-f0d9-40d7-a514-63c5db354e99.png"
-            style="filter: grayscale(100%)"
-          />
+          <img src="@/Imgs/location_Jp4vBwWWQJdspq8YagE9_images_5cfb2eb6-f0d9-40d7-a514-63c5db354e99.png"
+            style="filter: grayscale(100%)" />
           <h3 class="mt-3">COMPLETE COMFORT</h3>
         </div>
         <div class="col-lg-4 col-md-6 col-sm-12 text-center">
-          <img
-            src="@/Imgs/location_Jp4vBwWWQJdspq8YagE9_images_1ba99a6e-b596-4206-b837-3005f3512f77.png"
-            style="filter: grayscale(100%)"
-          />
+          <img src="@/Imgs/location_Jp4vBwWWQJdspq8YagE9_images_1ba99a6e-b596-4206-b837-3005f3512f77.png"
+            style="filter: grayscale(100%)" />
           <h3 class="mt-3">KINDNESS FROM ALL STAFF</h3>
         </div>
         <div class="col-lg-4 col-md-6 col-sm-12 text-center">
-          <img
-            src="@/Imgs/location_Jp4vBwWWQJdspq8YagE9_images_fde222ab-9a7a-4bf4-b024-01758716c8a3.png"
-            style="filter: grayscale(100%)"
-          />
+          <img src="@/Imgs/location_Jp4vBwWWQJdspq8YagE9_images_fde222ab-9a7a-4bf4-b024-01758716c8a3.png"
+            style="filter: grayscale(100%)" />
           <h3 class="mt-3">NOURISHMENT</h3>
         </div>
       </div>
+
       <Head Text="why we're different" />
       <div class="row justify-content-center align-items-center gy-4 my-3">
         <div class="col-lg-4 col-md-6 col-sm-12">
           <div class="card text-center">
             <img src="@/Imgs/1d58d4da-62d8-4525-b76d-705ee20025ed.png" />
-            <h2 class="text-uppercase py-2">Detail Oriented</h2>
-            <p class="p-2">
+            <h3 class="text-uppercase py-2">Detail Oriented</h3>
+            <p class="pt-2 pb-5 mb-5 ">
               We provide in-home services that cater to our patients’ specific
               needs and offer a wide array of therapies to make this time as
               peaceful as possible.
@@ -64,8 +59,8 @@
         <div class="col-lg-4 col-md-6 col-sm-12">
           <div class="card text-center">
             <img src="@/Imgs/009b08c3-1a7d-4d27-be43-d03a7194ec62.png" />
-            <h2 class="text-uppercase py-2">COMPLETE CARE</h2>
-            <p class="p-2">
+            <h3 class="text-uppercase py-2">COMPLETE CARE</h3>
+            <p class="pt-2 pb-5 mb-5 ">
               We provide in-home services that cater to our patients’ specific
               needs and offer a wide array of therapies to make this time as
               peaceful as possible.
@@ -75,8 +70,8 @@
         <div class="col-lg-4 col-md-6 col-sm-12">
           <div class="card text-center">
             <img src="@/Imgs/1c20a021-8e24-4261-ba4f-ffef6a74aa47.png" />
-            <h2 class="text-uppercase py-2">HERE FOR THE FAMILY</h2>
-            <p class="p-2">
+            <h3 class="text-uppercase py-2">HERE FOR THE FAMILY</h3>
+            <p class="pt-2 pb-5 mb-5 ">
               We provide in-home services that cater to our patients’ specific
               needs and offer a wide array of therapies to make this time as
               peaceful as possible.
@@ -104,16 +99,20 @@ export default {
 </script>
 <style lang="scss" scoped>
 @import "@/Scss/Main.scss";
+
 section {
   h3 {
     font-weight: 400;
   }
+
   .card {
     background-color: $Main-Color;
     color: white;
-    p{
+
+    p {
       line-height: 1.7;
     }
+
     img {
       width: 280px;
       height: 200px;
@@ -153,38 +152,49 @@ section {
   font-size: 18px;
   animation: call 1.5s infinite;
 }
+
 .btn:hover,
 .btn:active {
   border: 3px solid $Text-Color;
 }
+
 @keyframes call {
   15% {
     transform: translate(0px, 0%);
   }
+
   35% {
     transform: translate(0px, -25%);
   }
+
   45% {
     transform: translate(0px, 0%);
   }
+
   55% {
     transform: translate(0px, -15%);
   }
+
   70% {
     transform: translate(0px, 0%);
   }
+
   80% {
     transform: translate(0px, -10%);
   }
+
   90% {
     transform: translate(0px, 0%);
   }
+
   95% {
     transform: translate(0px, -2%);
   }
+
   99% {
     transform: translate(0px, 0%);
   }
+
   100% {
     transform: translate(0px, 0%);
   }
--- src/components/OurServeces/OurServeces.vue
+++ src/components/OurServeces/OurServeces.vue
@@ -1,5 +1,5 @@
 <template>
-    <section>
+    <section class="pb-4 mt-5">
         <div class="container">
             <div class="row header">
                 <div class="col-12">
@@ -12,15 +12,62 @@
                     </p>
                 </div>
             </div>
+            <div class="row gy-4 content mt-4">
+                <div class="col-lg-4 col-md-6 col-sm-12 text-center text-light" v-for="Service in Services">
+                    <img :src="require(`@/Imgs/${Service.ImgSrc}`)">
+                    <h4 class="py-3 text-uppercase">{{ Service.Header }}</h4>
+                    <p class="py-1 text-capitalize">{{ Service.Descrption }}</p>
+                </div>
+                <div class="text-center py-4">
+                    <PhoneButton PhoneNumber="(281)688-6783" />
+                </div>
+            </div>
         </div>
     </section>
 </template>
 <script>
 import Head from '../HeadEachSection/Head.vue';
+import PhoneButton from '../PhoneButton/PhoneButton.vue';
 
 export default {
     name: "OurServeces",
-    components: { Head }
+    components: { Head, PhoneButton },
+    data() {
+        return {
+            Services: [
+                {
+                    ImgSrc: 'location_Jp4vBwWWQJdspq8YagE9_images_8d6a83cf-7f30-487c-bd44-7bf28ddd5d6c.png',
+                    Header: 'WIDE SPECTRUM OF THERAPY',
+                    Descrption: 'Physical therapy, respiratory therapy, occupational therapy, and music therapy'
+                },
+                {
+                    ImgSrc: 'location_Jp4vBwWWQJdspq8YagE9_images_977f2275-843e-493d-91b3-ac86e81c1d62.png',
+                    Header: 'PHYSICAL COMFORT',
+                    Descrption: 'Pain treatment is given so patients can be emotionally and spiritually present.'
+                },
+                {
+                    ImgSrc: 'location_Jp4vBwWWQJdspq8YagE9_images_69f4f4cb-8555-472d-9e0e-afe539064d39.png',
+                    Header: 'EMOTIONAL AND SPIRITUAL CARE',
+                    Descrption: 'Get the emotional and spiritual support you need to get through this difficult time.'
+                },
+                {
+                    ImgSrc: 'location_Jp4vBwWWQJdspq8YagE9_images_9c865477-382d-46d7-8caa-0e3c47125b4a.png',
+                    Header: 'IN-HOME CARE',
+                    Descrption: 'We wll make arrangements to come to serve patients in the comfort of their own home.'
+                },
+                {
+                    ImgSrc: 'location_Jp4vBwWWQJdspq8YagE9_images_638c6cdd-63ad-4596-8b90-bc8a5d5cbbad.png',
+                    Header: 'Medical Staff ON-CALL',
+                    Descrption: 'Fully licensed staff is available 24/7'
+                },
+                {
+                    ImgSrc: 'location_Jp4vBwWWQJdspq8YagE9_images_b452c491-def7-4e72-97cb-3c1d31643d52.png',
+                    Header: 'EXPERT PHYSICIAN ADVICE + SUPPORT',
+                    Descrption: 'Our goal is to ease any suffering and your dedicated physician will help make informed recommendations.'
+                },
+            ]
+        }
+    },
 }
 </script>
 <style lang="scss" scoped>
@@ -32,6 +79,25 @@ section {
     .header {
         p {
             font-size: 25px;
+        }
+    }
+
+    .content {
+        img {
+            filter: grayscale(100%);
+            width: 130px;
+            height: 130px;
+            object-fit: contain;
+        }
+    }
+}
+
+@media only screen and (max-width:1025px) {
+    .header {
+        p {
+            span {
+                display: inline-block !important;
+            }
         }
     }
 }
